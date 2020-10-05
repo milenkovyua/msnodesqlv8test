eab62ed250287eb4b4a2d9aa28b138f7060c5332
Yuliyan Milenkov <milenkovyua@gmail.com>
2020-10-05 16:20:59 +0300

    chore: comments cleanup

--- testScripts/551-callSimpleDecimalProcedure.js
+++ testScripts/551-callSimpleDecimalProcedure.js
@@ -3,9 +3,7 @@ const sql = require(process.env.DB_DRIVER);
 
 module.exports = function callSimpleDecimalProcedure(pool) {
     const request = pool.request();
-    // const dec = sql.Decimal(19, 4);
-    // // dec.value=
-    request.input('dec', sql.Float(19, 4), 22.45); // could be called with sql.TVP  as second parameter and arrayList as third
-    // request.input('dec', sql.Decimal(19, 4), 22.45); // could be called with sql.TVP  as second parameter and arrayList as third
+    request.input('dec', sql.Float(19, 4), 22.45);
+    // request.input('dec', sql.Decimal(19, 4), 22.45);
     return request.execute('[testschema].[simple.decimal]');
 };
